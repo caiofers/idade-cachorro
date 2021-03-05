2797597f0f92d8570ae4619a0dcb2485deab13d6
Caio <caiovr13@gmail.com>
2021-03-05 00:16:24 -0300

    Primeiro app do curso de iOS Developer

--- PrimeiroApp/ViewController.swift
+++ PrimeiroApp/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBAction func descobrirIdade(_ sender: UIButton) {
+        if(!campoIdadeCachorro.text!.isEmpty){
+            legendaResultado.text = "A idade em anos humanos é: \(Int(campoIdadeCachorro.text!)! * 7)"
+        } else{
+            legendaResultado.text = "Au Au Au!"
+        }
+    }
+    
+    
+    @IBOutlet weak var campoIdadeCachorro: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("Idade Dog")
     }
 
 
